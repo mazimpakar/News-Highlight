3f2aac72b063075b7ea8c8328a5ffa253129ea85
mazimpakar <rosemazimpaka2@gmail.com>
2019-02-18 14:39:41 +0200

    root page function

--- app/views.py
+++ app/views.py
@@ -1,21 +1,21 @@
 from flask import render_template
 from app import app
 
-@app.route('/news/<int:news_id>')
-def news(news_id):
+# @app.route('/news/<int:news_id>')
+# def news(news_id):
 
-    '''
-    View news page function that returns the news details page and its data
-    '''
-    return render_template('news.html',id = news_id)
-def index():
+#     '''
+#     View news page function that returns the news details page and its data
+#     '''
+#     return render_template('news.html',id = news_id)
+# def index():
 
-    '''
-    View root page function that returns the index page and its data
-    '''
+#     '''
+#     View root page function that returns the index page and its data
+#     '''
 
-    title = 'Home - Welcome to The News Review Website Online'
-    return render_template('index.html', title = title)
+#     title = 'Home - Welcome to The News Review Website Online'
+#     return render_template('index.html', title = title)
 
 from .request import get_news
 
@@ -35,8 +35,9 @@ def index():
 
 
 
-@app.route('/')
-def index():
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
 
     '''
     View root page function that returns the index page and its data
@@ -44,7 +45,7 @@ def index():
 
     # Getting popular movie
     popular_news = get_news('popular')
-    mass_ media_news = get_news('mass media')
+    mass_media_news = get_news('mass media')
     newspapers_news = get_news('newspapers')
     title = 'Home - Welcome to News Review  Online'
-    return render_template('index.html', title = title, popular = popular_news, mass_media = mass _media_news, newspapers = newspapers_news)   
+    return render_template('index.html', title = title, popular = popular_news, mass_media = mass_media_news, newspapers = newspapers_news)   
