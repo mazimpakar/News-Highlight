2f139e355642d8aad3eb0c37225835e26d56ffdb
mazimpakar <rosemazimpaka2@gmail.com>
2019-02-18 14:35:11 +0200

    add news articles

--- app/news_test.py
+++ app/news_test.py
@@ -11,7 +11,7 @@ class NewsTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_news = News('Reuters','Lesley Wroughton',"Pompeo meets EU's top diplomat after Pence's Iran accusations - Reuters","https://s2.reutersmedia.net/resources/r/?m=02&d=20190215&t=2&i=1356813496&w=1200&r=LYNXNPEF1E0ID","U.S. Secretary of State Mike Pompeo met with the EU's top diplomat in Brussels on Friday, a day after Vice President Mike Pence accused America's traditional European allies of trying to undermine U.S. sanctions against Iran.""U.S. Secretary of State Mike Pompeo met with the EU's top diplomat in Brussels on Friday, a day after Vice President Mike Pence accused America's traditional European allies of trying to undermine U.S. sanctions against Iran.","2019-02-15T08:15:00Z")
+        self.new_news = News('The Hindu','Nikhil Varma and Divya Kala Bhavani',"So, let’s talk tech de-addiction centres","https://www.thehindu.com/sci-tech/technology/dx56fs/article26303250.ece/ALTERNATES/LANDSCAPE_615/19bgm-video-controller-3366571920","Tech de-addiction centres around the world are giving a whole new meaning to the phrase ‘modern-day epidemics’ — and no, we aren’t talking about a regular old digital detox","2019-02-18T10:54:34Z")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
