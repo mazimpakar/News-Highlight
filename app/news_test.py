279d234d5b8c6c70b6f5cc665d7db252a0e9e1cd
mazimpakar <rosemazimpaka2@gmail.com>
2019-02-19 17:38:42 +0200

    navbar

--- app/news_test.py
+++ app/news_test.py
@@ -1,6 +1,25 @@
 import unittest
-from models import news
-News = news.News
+from models import source
+from app.models import News
+
+Source = source.Source
+
+class SourceTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Source class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Source('"abc-news','ABC News', "https://abcnews.go.com","general","en")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,))
+
+
+
 
 class NewsTest(unittest.TestCase):
     '''
@@ -15,6 +34,11 @@ class NewsTest(unittest.TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
+
+
+
+
+
 
 
 if __name__ == '__main__':
