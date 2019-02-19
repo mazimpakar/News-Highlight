e1eaf2d0661f09e9d2f13c229e7ab4fc260ae81e
mazimpakar <rosemazimpaka2@gmail.com>
2019-02-19 09:21:08 +0200

    news source

--- app/views.py
+++ app/views.py
@@ -28,24 +28,26 @@ def index():
 
     # Getting popular news
     popular_news = get_news('popular')
-    print(popular_news)
-    title = 'Home - Welcome to The News Review Online'
-    return render_template('index.html', title = title,popular = popular_news)
-
-
-
-
-@app.route('/news/<int:news_id>')
-def news(news_id):
-
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-
-    # Getting popular movie
-    popular_news = get_news('popular')
     mass_media_news = get_news('mass media')
     newspapers_news = get_news('newspapers')
-    title = 'Home - Welcome to News Review  Online'
-    return render_template('index.html', title = title, popular = popular_news, mass_media = mass_media_news, newspapers = newspapers_news)   
+    print(popular_news,mass_media_news,newspapers_news)
+    title = 'Home - Welcome to The News Review Online'
+    return render_template('index.html', title = title,popular = popular_news,mass_media = mass_media_news,newspapers = newspapers_news)
+
+
+
+
+@app.route('/news/<news_name>')
+def news(news_name):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    title = f'You are viewing {news_name}'
+    return render_template('index.html', name = name, author = author,title = title,description = description, full_article = full_article,publishedAt = publishedAt ) 
+    
+
+    
+    
+
+
